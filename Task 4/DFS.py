c3832b3a1d627a0425448a3de44aaafe6f0557aa
collotech007q <kipkiruic007@gmail.com>
2026-05-15 15:44:37 +0300

    Add header comments with author and course details

--- Task 4/DFS.py
+++ Task 4/DFS.py
@@ -1,3 +1,9 @@
+#Name:   Collins Kipkirui
+#Reg No: CIT-227-065/2024
+#Unit:   Foundations of AI
+#Course: Software Engineering
+
+
 # Graph represented as an adjacency list
 graph = {
     'A': ['B', 'C'],
